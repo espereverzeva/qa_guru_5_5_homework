25d105eb213f23bdd0e71d5deaa4370dbbd8662d
espereverzeva <katya_novikova_1990_29@mail.ru>
2021-04-09 13:08:43 +0700

    Add

---
+++ src/test/java/tests/AnnotatedStepTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedStepTest {
+    private final static String REPOSITORY = "eroshenkoam/allure-example";
+    private final static int ISSUE_NUMBER = 68;
+
+   private WebSteps steps = new WebSteps();
+
+    @Test
+    public void testSearch() {
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.goToRepository(REPOSITORY);
+        steps.clickOnIssueTab();
+        steps.shouldSeeIssueWithNumber(ISSUE_NUMBER);
+    }
+}
